b31e9469d5dfcb6989931105277f7bbdf4a99f35
RyanJo3 <127467207+RyanJo3@users.noreply.github.com>
2023-04-21 15:16:02 +0900

    Update 로프.py

--- 현태/greedy/로프.py
+++ 현태/greedy/로프.py
@@ -11,3 +11,20 @@ for i in range(n):
         max_weight = weight
 
 print(max_weight)
+
+
+
+import sys 
+
+input = sys.stdin.readline
+
+n = int(input())
+ropes = []
+for i in range(n):
+    ropes.append(int(input()))
+ropes.sort(reverse=True)
+weight = []
+for i in range(n):  
+    weight.append(ropes[i]*(i+1))
+
+print(max(weight))
