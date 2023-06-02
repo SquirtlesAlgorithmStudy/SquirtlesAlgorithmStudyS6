ec1cb5e62daf852e4407e0968037091c46574b2a
mcjeong95 <mcjeong@postech.ac.kr>
2023-06-02 14:00:12 +0900

    백준 7562, 16234

---
+++ 민철/DFS_BFS/16234_인구이동.py
@@ -0,0 +1,56 @@
+from collections import deque
+n, l, r = map(int, input().split())
+
+graph = []
+answer = 0
+for i in range(n):
+    graph.append(list(map(int, input().split())))
+
+#상, 하, 좌, 우
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+def bfs(x, y, moved):
+    queue = deque()
+    queue.append((x, y))
+    federation = [(x, y)]
+    population_sum = graph[x][y]
+
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            
+            if nx < 0 or nx >= n or ny < 0 or ny >= n or visited[nx][ny]:
+                continue
+
+            if l <= abs(graph[nx][ny] - graph[x][y]) <= r:
+                visited[nx][ny] = 1
+                federation.append((nx, ny))
+                population_sum += graph[nx][ny]
+                queue.append((nx, ny))
+                moved = True
+            else:
+                continue
+        
+    for x, y in federation:
+        graph[x][y] = population_sum // len(federation)
+
+    return moved
+            
+while True:
+    moved = False
+    visited = [[0] * n for _ in range(n)]
+
+    for i in range(n):
+        for j in range(n):
+            if not visited[i][j]:
+                visited[i][j] = 1
+                moved = bfs(i, j, moved)
+    if not moved:
+        break
+    else:
+        answer += 1
+
+print(answer)
---
+++ 민철/DFS_BFS/7562_나이트의 이동.py
@@ -0,0 +1,35 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+def bfs(x, y):
+    dx = [-1, -2, -2, -1, 1, 2, 2, 1]
+    dy = [-2, -1, -1, -2, 2, 1, 1, 2]
+
+    queue = deque()
+    queue.append((x, y))
+    while queue :
+        x, y = queue.popleft()
+
+        if x == e_x and y == e_y:
+            return graph[x][y]
+        
+        for i in range(len(dx)) :
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if nx < 0 or nx >= l or ny < 0 or ny >= l:
+                continue
+
+            if graph[nx][ny] == 0:
+                graph[nx][ny] = graph[x][y] + 1
+                queue.append((nx,ny))
+                
+case_count = int(input().rstrip())    
+        
+for case in range(case_count) :
+    l = int(input().rstrip())
+    s_x, s_y = map(int, input().rstrip().split())
+    e_x, e_y = map(int, input().rstrip().split())
+    graph = [[0] * l for _ in range(l)]
+    print(bfs(s_x, s_y))
